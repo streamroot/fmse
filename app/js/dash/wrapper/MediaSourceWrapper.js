4cca9d5877b18c62dbe7241764f35435aa735dc3
pierrepak <kouangpierre@gmail.com>
2014-08-18 14:26:10 +0200

    update for require of MediaSourceWrapper

--- app/js/dash/wrapper/MediaSourceWrapper.js
+++ app/js/dash/wrapper/MediaSourceWrapper.js
@@ -1,15 +1,29 @@
+"use strict";
+
 MediaSourceWrapper = function () {
 
+	if (window.MediaSource !== null) {
+		_mediaSource = new MediaSource();
+		console.log("unprefixed MSE");
+		return _mediaSource
+	} else if(window.webkitMediaSource = window.WebKitMediaSource || window.webkitMediaSource || window.MozMediaSource) {
+		_mediaSource = new webkitMediaSource();
+		return _mediaSource
+	} else {
+		throw("Media Source Not available");
+	}
+
+	var self = this,
 	_init 		= function(){
 		this.readyState = 'open'
 		this.trigger({{type:'sourceopen'}})
-	}
+	},
 	
-	_listeners 	= {};
+	_listeners 	= {},
 
-	_duration 	= NaN; //
-	_readyState = 'close'; //close, open, ended
-	_sourceBuffers = [];
+	_duration 	= NaN, //
+	_readyState = 'close', //close, open, ended
+	_sourceBuffers = [],
 
 	_addEventListener 	= function(type, listener){
 		if (!this._listeners[type]){
@@ -42,10 +56,13 @@ MediaSourceWrapper = function () {
 		sourceBuffer = new SourceBuffer(this);
 		_sourceBuffers.push(sourceBuffer);
 		return sourceBuffer;
-	};
-	_removeSourceBuffer 	= function(){}
-	_endOfStream 			= function(){}
+	},
+	_removeSourceBuffer 	= function(){},
+	_endOfStream 			= function(){};
 	
 	init();
+	return self;
 
 }
+
+module.exports = MediaSourceWrapper;
