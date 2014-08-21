13814d38330d39955d4065cb6cce40e9671c1c1b
pierrepak <kouangpierre@gmail.com>
2014-08-21 14:17:36 +0200

    new rewrite MediaSourceWrapper

--- app/js/dash/wrapper/MediaSourceWrapper.js
+++ app/js/dash/wrapper/MediaSourceWrapper.js
@@ -1,79 +1,76 @@
 "use strict";
-var SourceBuffer = require('./SourceBuffer');
+var SourceBuffer 	= require('./SourceBuffer');
+
 
 var MediaSourceWrapper = function () {
-
-	if (window.MediaSource !== null) {
-		_mediaSource = new MediaSource();
-		console.log("unprefixed MSE");
-		return _mediaSource
-	} else if(window.webkitMediaSource = window.WebKitMediaSource || window.webkitMediaSource || window.MozMediaSource) {
-		_mediaSource = new webkitMediaSource();
-		return _mediaSource
-	} else {
-		throw("Media Source Not available");
-	}
-
-	var self = this,
-	_init 		= function(){
-		_readyState 	= 'open'
-		this.readyState = _readyState;
-		this.swfobject 	= _swfobject;
-		this.trigger({{type:'sourceopen'}})
-	},
-	
-	_listeners 	= {},
-
-	_duration 	= NaN, //
-	_readyState = 'close', //close, open, ended
-	_sourceBuffers = [],
-
-	_addEventListener 	= function(type, listener){
-		if (!this._listeners[type]){
-			this._listeners[type] = [];
-		}
-		this._listeners[type].unshift(listener);
-	},
-	
-	_removeEventListener = function(type, listener){
-		var listeners = this._listeners[type],
-			i = listeners.length;
-		while (i--) {
-			if (listeners[i] === listener) {
-				return listeners.splice(i, 1);
+	var
+	_mediasourceflash = {
+		srcUrl :'http://flash',
+		readyState: 'close',
+		swfobject:{},
+		duration:0,
+		_listeners:[],
+		_sourceBuffers:[],
+		_addEventListener:function(type, listener){
+			if (!this._listeners[type]){
+				this._listeners[type] = [];
 			}
-		}
-	},
-	
-	_trigger 			= function(event){
-		//updateend, updatestart
-		var listeners = this._listeners[event.type] || [],
-			i = listeners.length;
-		while (i--) {
-			listeners[i](event);
-		}
-	},
+			this._listeners[type].unshift(listener);
+		},
+		_removeEventListener: function(type, listener){
+			var listeners = this._listeners[type],
+				i = listeners.length;
+			while (i--) {
+				if (listeners[i] === listener) {
+					return listeners.splice(i, 1);
+				}
+			}
+		},
+		trigger :function(event){
+			//updateend, updatestart
+			var listeners = this._listeners[event.type] || [],
+				i = listeners.length;
+			while (i--) {
+				listeners[i](event);
+			}
+		},
+		addSourceBuffer: function(type){
+			var sourceBuffer;
+			sourceBuffer = new SourceBuffer(this, type);
+			_sourceBuffers.push(sourceBuffer);
+			return sourceBuffer;
+		},
+		removeSourceBuffer:function(){},
+		endOfStream: function(){},
+		initCallback:function (e){
+			swfobject = e.ref;
+			setTimeout(function(){
+				init();
+			},1500);
+		},
+		init:function(){
+			this.readyState = 'open'; 
+			//swfobject.embedSWF("pluginPlayer.swf", "video", "100%", "100%", "10.0.0", false, false, false, false, initCallback);
+			//to trigger when the flash shim is loaded
+			//this.trigger({{type:'sourceopen'}})
+			},
+		
 
-	_addSourceBuffer 		= function(type){
-		var sourceBuffer;
-		sourceBuffer = new SourceBuffer(this, type, swfobj);
-		_sourceBuffers.push(sourceBuffer);
-		return sourceBuffer;
-	},
-	_removeSourceBuffer 	= function(){},
-	_endOfStream 			= function(){};
+		
+	};
 	
-	_callback 	= function (e){
-		_swfobj = e.ref;
-		setTimeout(function(){
-			init();
-		},1500);
+	if (window.MediaSource !== null) {
+		console.log("unprefixed MSE 111");
+		_mediasourceflash.init();
+		return _mediasourceflash;
+		
+	} else if(window.webkitMediaSource = window.WebKitMediaSource || window.webkitMediaSource || window.MozMediaSource) {
+		console.log("window.webkitMediaSource");
+		return {}
+	} else {
+		console.log("Media Source Not available");
+		return {}
 	}
-	swfobject.embedSWF("pluginPlayer.swf", "flash_blob", "450", "600", "10.0.0", false, false, false, false, _callback);
-
-	
-	return self;
-
 }
 
 module.exports = MediaSourceWrapper;
