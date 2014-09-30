cd39cd55868fe21f17edc452c278f618b43ba739
axel <axel@streamroot.io>
2014-09-30 18:00:51 -0400

    NOT WORKING: MSE switch flash / HTML5
    Signed-off-by: axel <axel@streamroot.io>

--- app/js/dash/wrapper/MediaSourceWrapper_rewrite.js
+++ app/js/dash/wrapper/MediaSourceWrapper_rewrite.js
@@ -3,23 +3,39 @@
 var MediaSourceFlash = require('./MediaSourceFlash');
 
 var MediaSourceWrapper = function (video) {
-    return new MediaSourceFlash(video);
-    
-    //TODO: return real MSE if chrome, and flash instead. Implement conf parameter to use flash only.
-    /*
-    if (window.MediaSource !== null) {
-		console.log("unprefixed MSE 111");
-		_mediasourceflash.init();
-		return _mediasourceflash;
-		
-	} else if(window.webkitMediaSource = window.WebKitMediaSource || window.webkitMediaSource || window.MozMediaSource) {
-		console.log("window.webkitMediaSource");
-		return new webkitMediaSource();
-	} else {
-		console.log("Media Source Not available");
-		return {}
-	}
-    */
+    if (typeof video.getSwf !== "undefined") {
+        
+        //Flash shim
+        return new MediaSourceFlash(video);
+        
+    } else {
+        
+        //Using real media source. (case where MediaSource is not defined should be handled in wrapper. here it will not fallback to flash
+        //since SWF is not initialized in our library).
+        var mediaSource;
+        if (window.MediaSource !== null) {
+            //Unprefixed MSE
+            console.log("unprefixed MSE");
+            mediaSource = new MediaSource();
+        } else if(window.webkitMediaSource = window.WebKitMediaSource || window.webkitMediaSource || window.MozMediaSource) {
+            //Prefixed MSE
+            console.log("window.webkitMediaSource");
+            mediaSource = new webkitMediaSource();
+        } else {
+            console.log("Media Source Not available");
+            return {};
+        }
+        
+        //mediaSource is defined here, we already called return if if doesn't exist
+        //Set video.src BEFORE returning MSE
+        if (window.URL !== null) {
+            video.src = window.URL.createObjectURL(mediaSource);
+        } else if (window.webkitURL = window.webkitURL || window.WebKitURL) {
+            video.src = window.webkitURL.createObjectURL(mediaSource);
+        }
+        
+        return mediaSource;
+    }
 };
 
 module.exports = MediaSourceWrapper;
