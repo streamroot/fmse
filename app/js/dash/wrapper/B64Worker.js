cff68c328668d96432b0d0764bd309d9fb7a13ba
axel <axel@streamroot.io>
2014-11-04 12:28:18 -0500

    Loop in worker to encode data in Base 64
    Signed-off-by: axel <axel@streamroot.io>

--- app/js/dash/wrapper/B64Worker.js
+++ app/js/dash/wrapper/B64Worker.js
@@ -1,14 +1,28 @@
-var _arrayBufferToBase64 = function(buffer){
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
+var _bytes,
+    _b64Data,
+    _offset,
+    PIECE_SIZE = 65536;
+
+var _arrayBufferToBase64 = function(){
+    var i,
+        len = _bytes.byteLength,
+        end = Math.min(_offset + PIECE_SIZE, len);
+    for (i = _offset; i < end; i++) {
+        _b64Data += String.fromCharCode( _bytes[i] );
     }
-    return self.btoa(binary);
-};	
+    if (end === len) {
+        _b64Data = btoa(_b64Data);
+        self.postMessage({b64Data: _b64Data});
+    } else {
+        _offset = end;
+        setTimeout(_arrayBufferToBase64, 5);
+    }
+};
 
 self.onmessage = function (e) {
-    var b64Data = _arrayBufferToBase64(e.data.data);
-    self.postMessage({b64Data: b64Data});
+    _bytes = new Uint8Array(e.data.data);
+    _b64Data = '';
+    _offset = 0;
+    
+    _arrayBufferToBase64();
 };
