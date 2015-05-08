7e36d9c5b20aa44c0ff66ad9045c27f6c1423123
Kevin Canévet <kevin@streamroot.io>
2015-05-08 15:00:23 -0400

    the gruntfile and buildPlayer.py only aims at building this sole repository, and let vjs and jwplayer live their own lifes

--- buildPlayer.py
+++ buildPlayer.py
@@ -21,8 +21,6 @@ log_pts = "false"
 swfversion = "17"
 targetPlayer = "11.4.0"
 color = True
-vjs = True
-jwp = True
 
 startTime = time.time()
 
@@ -34,8 +32,6 @@ def helpParam():
     print "\t--no-color : disable color"
     print "\t-v : verbose mode"
     print "\t-h : display this menu"
-    print "\t--vjs : only build videojs"
-    print "\t--jwp : only build jwplayer"
     print ""
     sys.exit(0)
     
@@ -73,10 +69,6 @@ if (len(sys.argv)>1):
             log_pts = "true"
         elif sys.argv[i] == "-v":
             verbose = True
-        elif sys.argv[i] == "--vjs":
-            jwp = False
-        elif sys.argv[i] == "--jwp":
-            vjs = False
         elif sys.argv[i] in ["--help","-h"]:
             helpParam()
         elif sys.argv[i] == "--no-color":
@@ -154,79 +146,6 @@ if not os.path.exists(MAIN_OUTPUT):
     sys.exit(0)
 else:
     printPurple(">> " + MAIN_OUTPUT + " has been generated, sr-flash is built")
-
-if vjs:
-    if (os.path.exists(os.path.normpath("../streamroot-videojs/src/com/streamroot/mse.swc"))):
-        os.remove("../streamroot-videojs/src/com/streamroot/mse.swc")
-    shutil.copyfile(os.path.normpath(MAIN_OUTPUT),os.path.normpath("../streamroot-videojs/src/com/streamroot/mse.swc"))
-    printPurple(">> " + MAIN_OUTPUT + " has been copied in VideoJS directory")
-        
-if jwp:
-    if (os.path.exists(os.path.normpath("../streamroot-jwplayer/src/flash/com/streamroot/mse.swc"))):
-        os.remove(os.path.normpath("../streamroot-jwplayer/src/flash/com/streamroot/mse.swc"))
-    shutil.copyfile(os.path.normpath(MAIN_OUTPUT),os.path.normpath("../streamroot-jwplayer/src/flash/com/streamroot/mse.swc"))
-    printPurple(">> " + MAIN_OUTPUT + " has been copied in JWPlayer directory")
-
-#Moving to the jwplayer folder and compiling jwplayer
-if jwp:
-    os.chdir(os.path.normpath("../streamroot-jwplayer"))
-    if os.path.exists(JWP_OUTPUT):
-        os.remove(os.path.normpath(JWP_OUTPUT))
-    jwpResult = subprocess.Popen([os.path.normpath(flex + "/bin/mxmlc" + exe),
-                              os.path.normpath("src/flash/com/longtailvideo/jwplayer/player/Player.as"),
-                              "-compiler.source-path=src/flash",
-                              os.path.normpath("-compiler.library-path="+flex+"/frameworks/libs"),
-                              "-static-link-runtime-shared-libraries=true",
-                              os.path.normpath("-library-path=src/flash/com/streamroot/mse.swc"),
-                              "-default-background-color=0x000000",
-                              "-default-frame-rate=30",
-                              "-target-player="+targetPlayer+"",
-                              "-swf-version="+swfversion+"",
-                              "-debug="+debug+"",
-                              "-use-network=false",
-                              os.path.normpath("-output="+ JWP_OUTPUT),
-                              "-compiler.optimize=true",
-                              "-compiler.omit-trace-statements=true",
-                              "-warnings=false",
-                              "-define+=JWPLAYER::version,'6.11.20141118195350280'",
-                              "-define+=CONFIG::debugging,"+debug+""], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    popenPrint(jwpResult)
-    if not os.path.exists(JWP_OUTPUT):
-        printRed("Build failed")
-        sys.exit(0)
-    else:
-        shutil.copy2(os.path.normpath(JWP_OUTPUT),os.path.normpath("../sr-client-last/player_wrapper/jwplayer-wrapper/dist/6.8/jwplayer.srflash.swf"))
-        printPurple(">> " + "streamroot-jwplayer/" + JWP_OUTPUT + " has been generated, JWPlayer is built")
-    
-#Moving to the videojs folder and compiling videojs
-if vjs:
-    os.chdir(os.path.normpath("../streamroot-videojs"))
-    if os.path.exists(VJS_OUTPUT):
-        os.remove(os.path.normpath(VJS_OUTPUT))
-    vjsResult = subprocess.Popen([os.path.normpath(flex +"/bin/mxmlc" + exe),
-                              os.path.normpath("src/com/videojs/VideoJS.as"),
-                              "-compiler.source-path=src",
-                              "-compiler.library-path="+flex+"/frameworks/libs",
-                              os.path.normpath("-library-path=src/com/streamroot/mse.swc"),
-                              "-default-background-color=0x000000",
-                              "-default-frame-rate=30",
-                              "-target-player="+targetPlayer+"",
-                              "-swf-version="+swfversion+"",
-                              "-debug="+debug+"",
-                              "-use-network=false",
-                              "-static-link-runtime-shared-libraries=true",
-                              os.path.normpath("-output=" + VJS_OUTPUT),
-                              "-compiler.optimize=true",
-                              "-compiler.omit-trace-statements=true",
-                              "-warnings=false",
-                              "-define+=CONFIG::version,\"'4.2.2'\""], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    popenPrint(vjsResult)
-    if not os.path.exists(VJS_OUTPUT):
-        printRed("Build failed")
-        sys.exit(0)
-    else:
-        shutil.copy2(os.path.normpath(VJS_OUTPUT),os.path.normpath("../sr-client-last/player_wrapper/video-js-wrapper/dist/video-js-sr.swf"))
-        printPurple(">> " + "streamroot-videojs/" + VJS_OUTPUT + " has been generated, VideoJS is built")
 
 printGreen("Build successful")
 time = time.time() - startTime
