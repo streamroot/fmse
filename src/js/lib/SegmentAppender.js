8e55eb735943449d89cd781273ddfd7782e661de
Axel Delmas <axel@streamroot.io>
2016-02-23 19:03:00 +0100

    Normalize indentation in SegmentAppender

--- src/js/lib/SegmentAppender.js
+++ src/js/lib/SegmentAppender.js
@@ -3,54 +3,54 @@
 var SegmentAppender = function(sourceBuffer, swfObj, b64Encoder) {
     var self = this,
 
-    _b64Encoder = b64Encoder,
+        _b64Encoder = b64Encoder,
 
-    _sourceBuffer = sourceBuffer,
-    _swfObj = swfObj,
+        _sourceBuffer = sourceBuffer,
+        _swfObj = swfObj,
 
-    _type,
-    _startTime,
-    _endTime,
-    _segmentType,
-    _discard = false, //prevent from appending decoded segment to swf obj during seeking (segment was already in B64 when we seeked)
-    _seeking = false, //prevent an appendBuffer during seeking (segment arrived after having seeked)
-    _isDecoding = false,
+        _type,
+        _startTime,
+        _endTime,
+        _segmentType,
+        _discard = false, //prevent from appending decoded segment to swf obj during seeking (segment was already in B64 when we seeked)
+        _seeking = false, //prevent an appendBuffer during seeking (segment arrived after having seeked)
+        _isDecoding = false,
 
-    //Before sending segment to flash we check first if we are seeking. If so, we don't append the decoded data.
-    _doAppend = function (data) {
-        _isDecoding = false;
-        if (!_discard) {
-            console.info("SegmentApender: DO append " + _type + "_startTime=" + _startTime);
+        //Before sending segment to flash we check first if we are seeking. If so, we don't append the decoded data.
+        _doAppend = function (data) {
+            _isDecoding = false;
+            if (!_discard) {
+                console.info("SegmentApender: DO append " + _type + "_startTime=" + _startTime);
 
-            var isInit = _segmentType !== undefined
-                            ? _segmentType == "InitializationSegment"
-                            : isNaN(_startTime) || (typeof _endTime !== 'undefined');
+                var isInit = _segmentType !== undefined
+                ? _segmentType == "InitializationSegment"
+                : isNaN(_startTime) || (typeof _endTime !== 'undefined');
 
-            _swfObj.appendBuffer(data, _type, isInit, _startTime, _endTime);
-        } else {
-            console.info("SegmentApender: discard data " + _type);
-            _discard = false;
-            _sourceBuffer.segmentFlushed();
-        }
-    },
+                _swfObj.appendBuffer(data, _type, isInit, _startTime, _endTime);
+            } else {
+                console.info("SegmentApender: discard data " + _type);
+                _discard = false;
+                _sourceBuffer.segmentFlushed();
+            }
+        },
 
-    _appendBuffer = function(data, type, startTime, endTime, segmentType) {
+        _appendBuffer = function(data, type, startTime, endTime, segmentType) {
 
-        if (!_seeking) {
-            _type = type;
-            _startTime = startTime;
-            _endTime = endTime;
-            _segmentType = segmentType;
+            if (!_seeking) {
+                _type = type;
+                _startTime = startTime;
+                _endTime = endTime;
+                _segmentType = segmentType;
 
-            console.info("SegmentApender: start decoding " + _type);
-            _isDecoding = true;
-            _b64Encoder.encodeData(data, _doAppend);
-        } else {
-            _sourceBuffer.segmentFlushed();
-        }
-    },
+                console.info("SegmentApender: start decoding " + _type);
+                _isDecoding = true;
+                _b64Encoder.encodeData(data, _doAppend);
+            } else {
+                _sourceBuffer.segmentFlushed();
+            }
+        },
 
-    _initialize = function() {};
+        _initialize = function() {};
 
     self.appendBuffer = _appendBuffer;
 
